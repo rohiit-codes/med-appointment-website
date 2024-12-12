0e57b6fb36b6469da287223dbfeeffe6ec842e98
rohiit-codes <iamtheonerohit@gmail.com>
2024-12-13 00:38:17 +0530

    Functioning of Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,30 +1,55 @@
 import React from 'react'
 import {assets} from "../assets/assets"
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 
 const Navbar = () => {
+
+const navigate = useNavigate('')
+
+const [showMenu, setShowMenu] = useState(false)
+const [token, settoken] = useState(true)
+
   return (
     <div className='flex items-center py-4 mb-5 text-sm border-b border-gray-400 justify-between'>
-        <img className='w-44 cursor-pointer' src={assets.logo} alt="" />
+        <img className='w-40 cursor-pointer' src={assets.logo} alt="" />
         <ul className='hidden md:flex items-start gap-5 font-medium '>
-            <NavLink>
+            <NavLink to= '/'>
                 <li className='py-1'>HOME</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
+                <hr className='border-none outline-none h-0.5 w-4/5 bg-primary m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/doctors'>
                 <li className='py-1'>ALL DOCTORS</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
+                <hr className='border-none outline-none h-0.5 w-4/5 bg-primary m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/about'>
                 <li className='py-1'>ABOUT</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
+                <hr className='border-none outline-none h-0.5 w-4/5 bg-primary m-auto hidden'/>
             </NavLink>
-            <NavLink>
+            <NavLink to='/contact'>
                 <li className='py-1'>CONTACT</li>
-                <hr className='border-none outline-none h-0.5 w-3/5 bg-primary m-auto'/>
+                <hr className='border-none outline-none h-0.5 w-4/5 bg-primary m-auto hidden'/>
             </NavLink>
         </ul>
-        <button>Create account</button>
+        <div className='flex items-center gap-4'>
+
+            {
+                token
+                ?<div className='flex items-center cursor-pointer gap-2 group relative'>
+                    <img className = 'w-8 rounded-full' src={assets.profile_pic} alt="" />
+                    <img className = 'w-2.5' src={assets.dropdown_icon} alt="" />
+                    <div className='absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block'>
+                        <div>
+                            <p>My Profile</p>
+                            <p>My Appointments</p>
+                            <p>Logout</p>
+                        </div>
+                    </div>
+                </div>
+                :<button onClick = {() => navigate('/login')} className='border-primary border-2 rounded-lg py-2 px-1.5'>Create account</button>
+            }
+            
+        </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... 
+    },
     extend: {
       colors: {
         'primary' : '#5f6FFF'
