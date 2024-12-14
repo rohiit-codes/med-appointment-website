c602bf4e2212a4fbb155c730d519de29f4cc3da7
rohiit-codes <iamtheonerohit@gmail.com>
2024-12-15 01:19:50 +0530

    Hero Section completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,20 +3,21 @@ import {assets} from "../assets/assets"
 
 const Header = () => {
   return (
-    <div className='bg-[#5F6FFF] flex rounded-md pr-6'>
+    <div className='bg-primary sm:h-[620px] flex flex-col md:flex-row flex-wrap rounded-lg px-6 md:px-10 '>
         {/* --------Left Side */}
-        <div>
-             <p>Book Appointment<br/>With Trusted Doctors</p>
-             <div>
-                <img src={assets.group_profiles} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors,<br/>schedule your appointment hassle-free.</p>
+        <div className='md:w-1/2 flex flex-col md:items-start items-center justify-center gap-4 py-10 m-auto sm:m-0 md:py-[10vw] md:mb-[-30px]'>
+             <p className='text-2xl text-center md:text-start md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight'>Book Appointment<br/>With Trusted Doctors</p>
+             <div className='flex flex-col md:flex-row items-center gap-3 text-sm sm:text-xs'>
+                <img className='w-26' src={assets.group_profiles} alt="" />
+                <p className='text-white text-center md:text-start'>Simply browse through our extensive list of trusted doctors,<br className='hidden md:block'/>schedule your appointment hassle-free.</p>
              </div>
-             <a href="">Book appointment <img src={assets.arrow_icon} alt="" /></a>
+             <a className='bg-white py-3.5 px-8 text-gray-600 rounded-full text-xs gap-2.5 flex flex-row items-center hover:scale-105 transition-all duration-300 m-auto md:m-0' 
+             href="#speciality">Book appointment <img className='w-3' src={assets.arrow_icon} alt="" /></a>
         </div>
 
         {/* ---------Right Side */}
-        <div>
-             <img src={assets.header_img} alt="" />
+        <div className='md:w-1/2 relative'>
+             <img className= 'w-full md:absolute bottom-0  h-auto rounded-lg' src={assets.header_img} alt="" />
         </div>
     </div>
   )
