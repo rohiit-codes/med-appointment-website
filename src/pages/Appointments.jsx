cca3d213d55b51e6417b448d46cf3ea22113b0a0
rohiit-codes <iamtheonerohit@gmail.com>
2024-12-21 23:57:34 +0530

    Some more minor changes

--- src/pages/Appointments.jsx
+++ src/pages/Appointments.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useParams } from 'react-router-dom'
+import { AppContext } from '../context/AppContext'
 
 const Appointments = () => {
 
 const {docId} = useParams()
+
+const {doctors} = useContext(AppContext)
 
   return (
     <div></div>
